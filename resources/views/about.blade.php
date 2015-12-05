b0044d73cffa8dfbd86a92960428c36ead432808
shishupal <shishupal.n@pixxeldigital.com>
2015-12-05 15:54:59 +0530

    First Commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body style="font-family: Helvetica Neue,Helvetica,Arial,sans-serif;">
+	<table width="600" align="center">
+		<tr>
+			<td><a href="http://events.jaipurrugsco.com/domotex-2016/"><img src="https://mails-assets.s3.amazonaws.com/jrc/Domotex/mailer1/logo.jpg" alt="Jaipur Rugs Co." /></a></td>
+		</tr>
+		<tr>
+			<td><hr /></td>
+		</tr>
+		<tr>
+			<td align="center"><a href="http://events.jaipurrugsco.com/domotex-2016/"><img src="https://mails-assets.s3.amazonaws.com/jrc/Domotex/mailer1/banner.jpg" alt="Domotex Banner" /></a></td>
+		</tr>
+		<tr>
+			<td>
+				<table width="600" align="center">
+					<tr>
+						<td width="252" align="center"><img src="https://mails-assets.s3.amazonaws.com/jrc/Domotex/mailer1/domotex.jpg" alt="" /></td>
+						<td><img src="https://mails-assets.s3.amazonaws.com/jrc/Domotex/mailer1/sep-line.png" alt="" /></td>
+						<td width="347" align="center">
+							<span style="color:#5f5f5f; font-size:17px; font-family: Helvetica Neue,Helvetica,Arial,sans-serif;">Register on the link below for your<br> free ticket to Domotex-2016</span><br><br>
+							<a href="link_url"style="font-size: 20px; color: white; background-color:#464646; padding:5px; text-decoration:none; font-family: Helvetica Neue,Helvetica,Arial,sans-serif;">Click To Register</a>
+						</td>
+					</tr>
+				</table>
+			</td>
+		</tr>
+		<tr height="12">
+			<td style="background-color:#464646;"></td>
+		</tr>
+		<tr>
+			<td>
+				<span style="color: gray; font-weight: bold; line-height: 10px; font-family: Helvetica Neue,Helvetica,Arial,sans-serif;">
+                    For inquiries, please get in touch at <a href="mailto:jaipur@jaipurrugsco.com" target="_blank" style="color: #DB5E00;font-weight: bold;">jaipur@jaipurrugsco.com</a><br>
+                    T: +91 141 3987400 / 2398948<br>
+                    G-250 Mansarovar Ind. Area, Jaipur-20, Rajasthan, India<br>
+					<a href="https://www.facebook.com/jaipurrugs" target="_blank" style="color: #DB5E00;font-weight: bold;">Facebook</a> I 
+					<a href="https://www.twitter.com/jaipurrugs" target="_blank" style="color: #DB5E00;font-weight: bold;">Twitter</a> I 
+					<a href="http://www.youtube.com/jaipurrugs" target="_blank" style="color: #DB5E00;font-weight: bold;">YouTube</a>
+				</span>
+			</td>
+		</tr>
+	</table>
+</body>
+</html>
